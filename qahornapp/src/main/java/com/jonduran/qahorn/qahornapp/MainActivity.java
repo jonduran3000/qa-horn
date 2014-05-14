b1dfdfeb5c6e57a0a1804d0deb3e213ba2d3efc0
jonduran3000 <jonduran3000@gmail.com>
2014-05-13 21:53:56 -0400

    Initial commit

---
+++ qahornapp/src/main/java/com/jonduran/qahorn/qahornapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.jonduran.qahorn.qahornapp;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getActionBar().hide();
+
+        final float count = 100 * .01f;
+
+        final AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        final int originalVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+
+        final ImageButton hornButton = (ImageButton)this.findViewById(R.id.horn_button);
+        hornButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.inception);
+                mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                //mp.setVolume(count, count);
+                mp.start();
+            }
+        });
+
+        Switch switch1 = (Switch)this.findViewById(R.id.switch1);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    hornButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.media1);
+                            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                            //mp.setVolume(count, count);
+                            mp.start();
+                        }
+                    });
+                } else {
+                    hornButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.inception);
+                            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                            //mp.setVolume(count, count);
+                            mp.start();
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+}
